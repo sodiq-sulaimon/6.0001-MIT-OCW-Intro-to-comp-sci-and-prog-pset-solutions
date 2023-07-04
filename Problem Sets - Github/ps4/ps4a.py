2917424c8f4b62760d901bc409ab5f8862aa2cab
Sodiq Sulaimon <111898006+sodiq-sulaimon@users.noreply.github.com>
2023-07-03 19:31:09 -0500

    Update ps4a.py

--- Problem Sets - Github/ps4/ps4a.py
+++ Problem Sets - Github/ps4/ps4a.py
@@ -35,17 +35,17 @@ def get_permutations(sequence):
 if __name__ == '__main__':
 #    #EXAMPLE
    example_input = 'abc'
-   print('Input:', example_input)
-   print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-   print('Actual Output:', get_permutations(example_input))
+    print('Input:', example_input)
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(example_input))
     
 #    # Put three example test cases here (for your sanity, limit your inputs
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
 
-print("---------------------------------")
-print("Other examples:")
-print("Input: cat - ", get_permutations("cat"))
-print("Input: sun - ", get_permutations("sun"))
-print("Input: tap - ", get_permutations("tap"))
+    print("---------------------------------")
+    print("Other examples:")
+    print("Input: cat - ", get_permutations("cat"))
+    print("Input: sun - ", get_permutations("sun"))
+    print("Input: tap - ", get_permutations("tap"))
 
