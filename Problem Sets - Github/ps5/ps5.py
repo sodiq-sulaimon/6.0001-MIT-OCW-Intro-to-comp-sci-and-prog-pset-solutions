797c4888139cea5b8b3fc7039156d49914960af9
Sodiq Sulaimon <111898006+sodiq-sulaimon@users.noreply.github.com>
2023-07-28 11:31:24 -0500

    Add files via upload

---
+++ Problem Sets - Github/ps5/ps5.py
@@ -0,0 +1,322 @@
+# 6.0001/6.00 Problem Set 5 - RSS Feed Filter
+# Name: Sodiq Sulaimon
+# Collaborators:
+# Time:
+
+import feedparser
+import string
+import time
+import threading
+from project_util import translate_html
+from mtTkinter import *
+from datetime import datetime
+import pytz
+
+
+#-----------------------------------------------------------------------
+
+#======================
+# Code for retrieving and parsing
+# Google and Yahoo News feeds
+# Do not change this code
+#======================
+
+def process(url):
+    """
+    Fetches news items from the rss url and parses them.
+    Returns a list of NewsStory-s.
+    """
+    feed = feedparser.parse(url)
+    entries = feed.entries
+    ret = []
+    for entry in entries:
+        guid = entry.guid
+        title = translate_html(entry.title)
+        link = entry.link
+        description = translate_html(entry.description)
+        pubdate = translate_html(entry.published)
+
+        try:
+            pubdate = datetime.strptime(pubdate, "%a, %d %b %Y %H:%M:%S %Z")
+            pubdate.replace(tzinfo=pytz.timezone("GMT"))
+          #  pubdate = pubdate.astimezone(pytz.timezone('EST'))
+          #  pubdate.replace(tzinfo=None)
+        except ValueError:
+            pubdate = datetime.strptime(pubdate, "%a, %d %b %Y %H:%M:%S %z")
+
+        newsStory = NewsStory(guid, title, description, link, pubdate)
+        ret.append(newsStory)
+    return ret
+
+#======================
+# Data structure design
+#======================
+
+# Problem 1
+
+# TODO: NewsStory
+
+class NewsStory(object):
+    def __init__(self, guid, title, description, link, pubdate):
+        self.guid = guid
+        self.title = title
+        self.description = description
+        self.link = link
+        self.pubdate = pubdate
+    
+    def get_guid(self):
+        """Used to safely access guid"""
+        return self.guid
+    
+    def get_title(self):
+        """Used to safely access title"""
+        return self.title
+    
+    def get_description(self):
+        """Used to safely access description"""
+        return self.description
+    
+    def get_link(self):
+        """Used to safely access link"""
+        return self.link
+    
+    def get_pubdate(self):
+        """Used to safely access pubdate"""
+        return self.pubdate
+
+
+#======================
+# Triggers
+#======================
+
+class Trigger(object):
+    def evaluate(self, story):
+        """
+        Returns True if an alert should be generated
+        for the given news item, or False otherwise.
+        """
+        # DO NOT CHANGE THIS!
+        raise NotImplementedError
+
+# PHRASE TRIGGERS
+
+# Problem 2
+# TODO: PhraseTrigger
+class PhraseTrigger(Trigger):
+    def __init__(self, phrase):
+        self.phrase = phrase.lower()
+    
+    def is_phrase_in(self, text):
+        """Takes in one string argument text and returns True if the 
+        whole phrase is present in text, False otherwise"""
+        
+        clean_phrase = ''
+        for word in self.phrase:
+            for char in word:
+                if char not in string.punctuation:
+                    clean_phrase += char
+        clean_text = ''
+        for word in self.text.lower():
+            for char in word:
+                if char not in string.punctuation:
+                    clean_text += char
+        
+        if clean_phrase in clean_text:
+            return True
+        else:
+            return False
+
+                    
+# Problem 3
+# TODO: TitleTrigger
+class TitleTrigger(PhraseTrigger):
+    """Fires when a news item's title contains a given phrase"""
+    def evaluate(self, story):
+        return self.is_phrase_in(story.get_title())
+    
+    
+# Problem 4
+# TODO: DescriptionTrigger
+class DescriptionTrigger(PhraseTrigger):
+    def evaluate(self, story):
+        return self.is_phrase_in(story.get_description())
+
+# TIME TRIGGERS
+
+# Problem 5
+# TODO: TimeTrigger
+# Constructor:
+#        Input: Time has to be in EST and in the format of "%d %b %Y %H:%M:%S".
+#        Convert time from string to a datetime before saving it as an attribute.
+class TimeTrigger(Trigger):
+    def __init__(self, date_time):
+        self.date_time = datetime.strptime(date_time, "%d %b %Y %H:%M:%S")
+
+# Problem 6
+# TODO: BeforeTrigger and AfterTrigger
+class BeforeTrigger(TimeTrigger):
+    """Fires when a story is published strictly before the trigger’s time"""
+    def evaluate(self, newsStory):
+        if newsStory.get_pubdate() < self.date_time:
+            return True
+        return False
+
+class AfterTrigger(TimeTrigger):
+    """Fires when a story is published strictly after the trigger’s time"""
+    def evaluate(self, newsStory):
+        if newsStory.get_pubdate() > self.date_time:
+            return True
+        return False
+
+# COMPOSITE TRIGGERS
+
+# Problem 7
+# TODO: NotTrigger
+class NotTrigger(Trigger):
+    """This trigger produces its output by inverting the output of another trigger"""
+    def __init__(self, trigger):
+        self.trigger = trigger
+        
+    def evaluate(self, story):
+        return not self.trigger.evaluate(story)
+# Problem 8
+# TODO: AndTrigger
+class AndTrigger(Trigger):
+    """This trigger takes two triggers as arguments to its constructor, and fires on a
+news story only if both of the inputted triggers would fire on that item"""
+    def __init__(self, trigger1, trigger2):
+        self.trigger1 = trigger1
+        self.trigger2 = trigger2
+    def evaluate(self, story):
+        return self.trigger1.evaluate(story) and self.trigger2.evaluate(story)
+
+# Problem 9
+# TODO: OrTrigger
+class OrTrigger(Trigger):
+    """This trigger takes two triggers as arguments to its constructor, and fires if either
+one (or both) of its inputted triggers would fire on that item."""
+    def __init__(self, trigger1, trigger2):
+        self.trigger1 = trigger1
+        self.trigger2 = trigger2
+    def evaluate(self, story):
+        return self.trigger1.evaluate(story) or self.trigger2.evaluate(story)
+
+
+#======================
+# Filtering
+#======================
+
+# Problem 10
+def filter_stories(stories, triggerlist):
+    """
+    Takes in a list of NewsStory instances.
+
+    Returns: a list of only the stories for which a trigger in triggerlist fires.
+    """
+    # TODO: Problem 10
+    # This is a placeholder
+    # (we're just returning all the stories, with no filtering)
+    return stories
+
+
+
+#======================
+# User-Specified Triggers
+#======================
+# Problem 11
+def read_trigger_config(filename):
+    """
+    filename: the name of a trigger configuration file
+
+    Returns: a list of trigger objects specified by the trigger configuration
+        file.
+    """
+    # We give you the code to read in the file and eliminate blank lines and
+    # comments. You don't need to know how it works for now!
+    trigger_file = open(filename, 'r')
+    lines = []
+    for line in trigger_file:
+        line = line.rstrip()
+        if not (len(line) == 0 or line.startswith('//')):
+            lines.append(line)
+
+    # TODO: Problem 11
+    # line is the list of lines that you need to parse and for which you need
+    # to build triggers
+
+    print(lines) # for now, print it so you see what it contains!
+
+
+
+SLEEPTIME = 120 #seconds -- how often we poll
+
+def main_thread(master):
+    # A sample trigger list - you might need to change the phrases to correspond
+    # to what is currently in the news
+    try:
+        t1 = TitleTrigger("election")
+        t2 = DescriptionTrigger("Trump")
+        t3 = DescriptionTrigger("Clinton")
+        t4 = AndTrigger(t2, t3)
+        triggerlist = [t1, t4]
+
+        # Problem 11
+        # TODO: After implementing read_trigger_config, uncomment this line 
+        # triggerlist = read_trigger_config('triggers.txt')
+        
+        # HELPER CODE - you don't need to understand this!
+        # Draws the popup window that displays the filtered stories
+        # Retrieves and filters the stories from the RSS feeds
+        frame = Frame(master)
+        frame.pack(side=BOTTOM)
+        scrollbar = Scrollbar(master)
+        scrollbar.pack(side=RIGHT,fill=Y)
+
+        t = "Google & Yahoo Top News"
+        title = StringVar()
+        title.set(t)
+        ttl = Label(master, textvariable=title, font=("Helvetica", 18))
+        ttl.pack(side=TOP)
+        cont = Text(master, font=("Helvetica",14), yscrollcommand=scrollbar.set)
+        cont.pack(side=BOTTOM)
+        cont.tag_config("title", justify='center')
+        button = Button(frame, text="Exit", command=root.destroy)
+        button.pack(side=BOTTOM)
+        guidShown = []
+        def get_cont(newstory):
+            if newstory.get_guid() not in guidShown:
+                cont.insert(END, newstory.get_title()+"\n", "title")
+                cont.insert(END, "\n---------------------------------------------------------------\n", "title")
+                cont.insert(END, newstory.get_description())
+                cont.insert(END, "\n*********************************************************************\n", "title")
+                guidShown.append(newstory.get_guid())
+
+        while True:
+
+            print("Polling . . .", end=' ')
+            # Get stories from Google's Top Stories RSS news feed
+            stories = process("http://news.google.com/news?output=rss")
+
+            # Get stories from Yahoo's Top Stories RSS news feed
+            stories.extend(process("http://news.yahoo.com/rss/topstories"))
+
+            stories = filter_stories(stories, triggerlist)
+
+            list(map(get_cont, stories))
+            scrollbar.config(command=cont.yview)
+
+
+            print("Sleeping...")
+            time.sleep(SLEEPTIME)
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title("Some RSS parser")
+    t = threading.Thread(target=main_thread, args=(root,))
+    t.start()
+    root.mainloop()
+
